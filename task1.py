8da6ca5d2a82d74fda1787748ace89a2dc162dc6
mimi-minnie <adobbs38@gmail.com>
2014-06-18 14:11:06 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,16 @@
 symbols = ['GBP', 'USD', 'EUR', 'JPY']
 rates = [1,2,3,4]
 
-def conv (value cfrom,cto):
-  answer = val / rates(cf)
-  answer = answer * rates(cb)
+def conv (value, cfrom,cto):
+  answer = value / rates[cfrom]
+  answer = answer * rates[cto]
   return (answer)
+
+def ask (direction):
+    ans=raw_input("please the conrentce you are converting {0}".format(direction))
+    return ans
+    
+def askv ():
+    ans=raw_input("please enter amount {0}".format())
+    return ans
+    
